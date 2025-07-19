8babbd0e53404b3c4086aa08ee2563e3765cfe03
bharadwaj devalaraju <bharadwajdevalraju007@gmail.com>
2025-07-19 15:33:40 +0530

    feat(blind75): set2

--- src/main/java/org/bd/blind50/set2/LongestRepeatingCharacterReplacement.java
+++ src/main/java/org/bd/blind50/set2/LongestRepeatingCharacterReplacement.java
@@ -1,4 +1,27 @@
 package org.bd.blind50.set2;
 
+import java.util.HashMap;
+
 public class LongestRepeatingCharacterReplacement {
+
+    public static int solve(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left=0;
+        int max =0;
+        int maxCount =0;
+        for(int right=0;right<s.length();right++){
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c,0)+1);
+            if(map.get(c)>maxCount) {
+                maxCount = map.get(c);
+            }
+            while((right-left+1)-maxCount > k){
+                char leftC = s.charAt(left);
+                map.put(leftC, map.get(leftC)-1);
+                left++;
+            }
+            max= Math.max(max, right-left+1);
+        }
+        return max;
+    }
 }
--- src/main/java/org/bd/blind50/set2/PacificAtlanticWaterFlow.java
+++ src/main/java/org/bd/blind50/set2/PacificAtlanticWaterFlow.java
@@ -1,4 +1,42 @@
 package org.bd.blind50.set2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PacificAtlanticWaterFlow {
+
+
+    public static int[][] solve(int[][] grid) {
+
+        List<int[]> list = new ArrayList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visitedP = new boolean[m][n];
+        boolean[][] visitedA = new boolean[m][n];
+        for(int i=0;i<n;i++) dfs(0, i, grid, visitedP, grid[0][i]);
+        for(int i=0;i<m;i++) dfs(i, 0, grid, visitedP, grid[i][0]);
+
+        for(int i=0;i<m;i++) dfs(i,n-1, grid, visitedA, grid[i][n-1]);
+        for (int i=0;i<n;i++) dfs(m-1, i, grid, visitedA, grid[m-1][i]);
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(visitedP[i][j] && visitedA[i][j]) {
+                    list.add(new int[]{i, j});
+                }
+            }
+        }
+        return list.toArray(new int[][]{});
+    }
+
+    private static void dfs(int i, int j, int[][] grid, boolean[][] visited, int prevHeight) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || visited[i][j] || prevHeight > grid[i][j]) {
+            return;
+        }
+        visited[i][j] = true;
+        dfs(i, j + 1, grid, visited, grid[i][j]);
+        dfs(i + 1, j, grid, visited, grid[i][j]);
+        dfs(i, j - 1, grid, visited, grid[i][j]);
+        dfs(i - 1, j, grid, visited, grid[i][j]);
+    }
 }
