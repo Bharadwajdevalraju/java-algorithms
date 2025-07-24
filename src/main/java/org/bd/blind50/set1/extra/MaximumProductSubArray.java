a35279ba706eb256d666d2a367451d3fdc7e4a63
bharadwaj devalaraju <bharadwajdevalraju007@gmail.com>
2025-07-24 22:19:11 +0530

    feat(blind75): set1 optional

--- src/main/java/org/bd/blind50/set1/extra/MaximumProductSubArray.java
+++ src/main/java/org/bd/blind50/set1/extra/MaximumProductSubArray.java
@@ -3,14 +3,23 @@ package org.bd.blind50.set1.extra;
 public class MaximumProductSubArray {
 
     public int solve(int[] nums) {
-        int max =1, min=1;
-        int result= 0;
+        int min=1, max=1;
+        int res = nums[0];
         for(int i=0;i<nums.length;i++) {
-            min = Math.min(min,nums[i] * min);
-            max = Math.max(max, nums[i] * max);
+            if (nums[i]==0) { // reset
+                min = 1;
+                max = 1;
+                res = Math.max(res, 0);
+                continue;
+            }
+            int minH = nums[i] * min;
+            int maxH = nums[i] * max;
+            min = Math.min(nums[i], Math.min(minH, maxH));
+            max = Math.max(nums[i], Math.max(minH, maxH));
+            res = Math.max(res, max);
 
         }
-        return 0;
+        return res;
     }
 
 }
